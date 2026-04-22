bbd5e8225dcdf9adca7619f6404298477af05632
blake125 <blakeranniker@gmail.com>
2026-04-22 09:38:26 -0700

    refactored camera controls, changed level 1 world environment

--- Assets/characters/character_body_3d.gd
+++ Assets/characters/character_body_3d.gd
@@ -4,27 +4,22 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 6.5
 const DUB_JUMP_VELOCITY = 10
-var has_double_jumped: bool = false
-const MOUSE_SENS = 0.01
 
-@onready var camera: Camera3D
-@onready var pivot: Node3D = $Pivot
+@onready var camera: Camera3D = $HPivot/SpringArm3D/Camera3D
+@onready var v_pivot: Node3D = $HPivot/SpringArm3D
+@onready var h_pivot: Node3D = $HPivot
 @onready var body = $the_chef_v2
+
+var has_double_jumped: bool = false
+var mouse_sens = 0.01
+
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		var motion: Vector2 = event.screen_relative
-		
-		var ymotion: float = motion.y
-		var xmotion: float = motion.x
-		
-		pivot.rotation.x +=ymotion * MOUSE_SENS
-		pivot.rotation.y -=xmotion * MOUSE_SENS
-		
-		body.rotation.y -=xmotion * MOUSE_SENS
+		handle_mouse_movement(event)
 		
 	if Input.is_action_just_pressed("leave_game"):
 		get_tree().quit()
@@ -37,7 +32,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		has_double_jumped = false
 
-	# Handle jump.
+	# Handle jump and double jump
 	if Input.is_action_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	elif Input.is_action_just_pressed("move_jump") and !has_double_jumped:
@@ -48,7 +43,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction := (pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (h_pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -57,3 +52,10 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+func handle_mouse_movement(event: InputEventMouseMotion):
+	var motion: Vector2 = event.relative
+	v_pivot.rotation.x -= motion.y * mouse_sens
+	h_pivot.rotation.y -= motion.x * mouse_sens
+		
+	body.rotation.y -= motion.x * mouse_sens
