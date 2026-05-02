323bbc289353425c48954fc1d73d3a209642a7fe
Danny Hoang <bajanboss132@gmail.com>
2026-05-01 21:16:58 -0700

    feat(level-switch) HOLY SHIT IT WORKS NOW

--- Globals/Components/scene_switch.gd
+++ Globals/Components/scene_switch.gd
@@ -1,7 +1,8 @@
 extends Node3D
 
 var interaction_controller : InteractableComponent
-@export var target_level : PackedScene
+@export_file_path var target_level
+@export var test : String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,15 +11,16 @@ func _ready() -> void:
 	interaction_controller.onInteraction.connect(_on_interaction)
 	# assert it is a Level. Otherwise, alert.
 	print("hello level 2 ")
+	print("test: ", test)
 	_print_target_level_warnings()
 
 
 func _on_interaction() -> void:
-	print("Scene Switch!!")
-	print(target_level.get_path())
+	#print("Scene Switch!!")
+	#print(target_level.get_path())
 	
 	var controller : GameController = get_node("/root/GameController")
-	controller.load_scene(target_level.get_path())
+	controller.load_scene(target_level)
 	
 	#GameController.load_scene(target_level.get_path())
 
@@ -28,14 +30,15 @@ func _print_target_level_warnings() -> void:
 		print("[WARN]: No target level specified for: ", self.get_path())
 		return
 		
-	var target_level_script : Script = target_level.get_state().get_node_property_value(0, 0)
-	
-	if (not target_level_script is Script):
-		print("[WARN] Target level does not inherit from Level:", self.get_path())
-		print("Got: ", typeof(target_level_script))
-		return
-		
 	#TODO: actually check class names or somthin
+		
+	#var target_level_script : Script = target_level.get_state().get_node_property_value(0, 0)
+	#
+	#if (not target_level_script is Script):
+		#print("[WARN] Target level does not inherit from Level:", self.get_path())
+		#print("Got: ", typeof(target_level_script))
+		#return
+		
 	#print(target_level_script.get_global_name())
 	#if (not target_level_script.get_global_name() ):
 		#print("[WARN] Target level inherits from unexpected class:", self.get_path())
