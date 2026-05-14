a9408a4b45d8485a182008580432e8ea1b5d1b89
Danny Hoang <bajanboss132@gmail.com>
2026-05-14 11:27:21 -0700

    feat(settings): animation on disable settings, perfect.

--- Globals/GUI/Settings/settings.gd
+++ Globals/GUI/Settings/settings.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@export var active : bool
 @onready var animation = $AnimationPlayer
 
 
@@ -8,6 +9,7 @@ extends Control
 #signal settings_toggled(settings_active : bool)
 
 func _ready() -> void:
+	active = visible
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	if visible:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
@@ -18,10 +20,13 @@ func _ready() -> void:
 # So it's actually always active, might be tanking performance?
 func _unhandled_input(event: InputEvent) -> void:
 	if (event is InputEventKey and event.is_action_pressed("ui_cancel")):
-		visible = ! visible;
+		#visible = ! visible;
+		active = ! active;
 		#settings_toggled.emit(visible)
-		if visible:
+		
+		if active:
 			# activate
+			visible = true
 			get_tree().paused = true
 			Global.mouse_mode_push(Input.mouse_mode)
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
@@ -31,4 +36,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			# deactivate
 			get_tree().paused = false
 			Global.mouse_mode_pop_and_apply()
+			animation.play("peace_out")
+			await animation.animation_finished
+			visible = false
 		get_viewport().set_input_as_handled()
